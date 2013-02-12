9ee2fa7042ee54949001651f3d6eb8fa2d56cda0
Thomas Levine <occurrence@thomaslevine.com>
2013-02-12 12:30:54 -0500

    all mail

--- content/!/fifteen/data.sh
+++ content/!/fifteen/data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-notmuch search --output=files $(date -d 2005-03-30 +%s)..$(date -d 2006-03-30 +%s) from:Thomas\ Levine|grep 'All Mail'|head > /tmp/filenames
+notmuch search --output=files $(date -d 2005-03-30 +%s)..$(date -d 2006-03-30 +%s) from:Thomas\ Levine|grep 'All Mail' > /tmp/filenames
 
 header() {
-  echo datetime,line.count,char.count
+  echo email,datetime,line.count,char.count
 }
 
 features() {
@@ -17,7 +17,10 @@ features() {
   rawdate=$(sed -n 's/^Received: by .*; //p' "$email")
   stddate=$(date -d "$rawdate" +'%Y-%m-%d %H:%M:%S')
 
-  echo "$stddate,$lines,$chars,"
+  from=$(sed -n 's/From: //p' "$email")
+  to=$(sed -n 's/To: //p' "$email")
+
+  echo "$email,$stddate,$lines,$chars,$from,$to"
 }
 
 header
