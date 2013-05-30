642eac724505cae903795e941fa0d3b3d19be0b1
Thomas Levine <occurrence@thomaslevine.com>
2013-05-29 22:50:32 -0700

    reorganize words list

--- content/js/_main.js
+++ content/js/_main.js
@@ -1,25 +1,25 @@
 //$(function(){
   var words = [
+    'hamster',
+    'paraphernalia',
+    'perluette',
+
     // http://marvin.cs.uidaho.edu/misspell.html
     'accessible',
     'accidentally',
     'definitely',
     'fuchsia',
-    'hamster',
     'mayonnaise',
     'mosquito',
-    'perluette',
     'occurrence',
     'orangutan',
     'pamphlet',
-    'paraphernalia',
     'syllable',
     'vinaigrette',
     'wildebeest',
     'zucchini',
-    'xylophone'
+    'xylophone',
   ]
-  // console.log(words.join('\nis '))
 
   // http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
   var word = words[Math.floor(Math.random()*words.length)]
