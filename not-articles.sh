23d779da206a2fc5f702a55182eb2dd83e0e8113
Thomas Levine <occurrence@thomaslevine.com>
2013-06-26 13:42:38 -0700

    not-articles.sh echos rather than vims

--- not-articles.sh
+++ not-articles.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for file in content/\!/*/index.md; do grep article "$file" || vim "$file"; done
+for file in content/\!/*/index.md; do grep article "$file" > /dev/null || echo "$file"; done
