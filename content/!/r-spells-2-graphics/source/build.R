27bd370d3462b3038f565f91ac665b2c4fdda9ca
Thomas Levine <occurrence@thomaslevine.com>
2013-08-02 11:40:41 -0700

    tweak build

--- content/!/r-spells-2-graphics/source/build.R
+++ content/!/r-spells-2-graphics/source/build.R
@@ -1,4 +1,9 @@
 library(knitr)
+library(sqldf)
+
 knit('index.Rmd')
 file.rename('index.md', '../index.md')
+if (file.exists('../figure')) {
+  file.remove('../figure')
+}
 file.rename('figure', '../figure')
