f82801bbb994b9b54ea7e2f6c8604cb2405552f2
Thomas Levine <occurrence@thomaslevine.com>
2013-10-13 19:07:53 +0000

    change email address

--- content/js/_main.js
+++ content/js/_main.js
@@ -1,40 +1,9 @@
 // Email address
 (function(){
-  var words = [
-    'hamster',
-    'paraphernalia',
-    'perluette',
-
-    // http://marvin.cs.uidaho.edu/misspell.html
-    'accessible',
-    'accidentally',
-    'definitely',
-    'fuchsia',
-    'mayonnaise',
-    'mosquito',
-    'occurrence',
-    'orangutan',
-    'pamphlet',
-    'syllable',
-    'vinaigrette',
-    'wildebeest',
-    'zucchini',
-    'xylophone',
-  ]
-
   // http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
-  var word = words[Math.floor(Math.random()*words.length)]
-  var chainsaw=[word,'thomaslevine.com'].join('@')
+  var chainsaw=['','@thomaslevine', 'com'].join('.')
   var epost = document.getElementById("e-post")
   if (epost){
     epost.innerHTML=chainsaw
   }
 })()
-
-/*
-$(function(){
-  setTimeout(function(){
-    $('#feedback').fadeIn()
-  }, 3 * 60 * 1000)
-})
-*/
