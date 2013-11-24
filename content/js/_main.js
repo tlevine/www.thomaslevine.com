98a273864edeb0ec7d4b35ab029e2e82ae1ed81e
Thomas Levine <occurrence@thomaslevine.com>
2013-11-24 06:12:35 +0000

    remove email pseudo-obfuscation per @isaacs's rant

--- content/js/_main.js
+++
@@ -1,9 +0,0 @@
-// Email address
-(function(){
-  // http://stackoverflow.com/questions/11935175/sampling-a-random-subset-from-an-array
-  var chainsaw=['','@thomaslevine', 'com'].join('.')
-  var epost = document.getElementById("e-post")
-  if (epost){
-    epost.innerHTML=chainsaw
-  }
-})()
